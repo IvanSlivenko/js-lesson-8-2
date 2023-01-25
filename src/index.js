6a2f75a8470df62c38607c4c3aa82e0ce859b92e
IvanSlivenko <ivan8822@ulr.net>
2023-01-25 21:02:53 +0200

    etap-25-01-2023-21-02

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// import * as basicLightbox from 'basiclightbox';
+
+// const Handlebars = require("handlebars");
+
+function Message(user) {
+console.log(`Hello ${user} `);    
+}
+Message('Ivan');
+
